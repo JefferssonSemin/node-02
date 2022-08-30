73c841cb1db5fce85c99a394fb686e05328d0067
JefferssonSemin <jeffesemin@hotmail.com>
2022-08-30 14:57:03 -0300

    feat: add specification module

---
+++ src/modules/cars/repositories/ISpecificationRepository.ts
@@ -0,0 +1,15 @@
+import { Category } from "../model/Category";
+
+
+interface ICreateSpecificationDTO{
+    name: string, 
+    description: string
+}
+
+interface ISpecificationRepository{
+    findByName(name:string): Category | undefined
+    list(): Category[]
+    create({name, description}:ICreateSpecificationDTO):void
+}
+
+export {ISpecificationRepository, ICreateSpecificationDTO}
