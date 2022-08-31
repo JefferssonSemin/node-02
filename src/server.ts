e744ca14fde1643f5a5b532c6d8faffed899ee4d
JefferssonSemin <jeffesemin@hotmail.com>
2022-08-30 22:46:13 -0300

    feat: add documentation with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from "express"
+import swaggerui from 'swagger-ui-express'
+
 import { router } from "./routes"
+import swaggerfile from './swagger.json'
+
 const app = express()
 
 app.use(express.json())
+
+app.use("/swagger", swaggerui.serve, swaggerui.setup(swaggerfile))
+
 app.use(router)
 
 app.listen(3333, () => console.log("server is running!"))
