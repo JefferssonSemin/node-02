1c613051fdc1313e073c87650a22a6b9618dc1d9
JefferssonSemin <jeffesemin@hotmail.com>
2022-09-02 18:15:09 -0300

    feat: add test to authenticate users

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,50 @@
+import { AppError } from "../../../../errors/AppError"
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO"
+import { UserRepositoryInMemory } from "../../repositories/in-memory/UserRepositoryInMemory"
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase"
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
+
+let authenticateUserUseCase: AuthenticateUserUseCase
+let createUserUseCase: CreateUserUseCase
+let userRepositoryInmemory: UserRepositoryInMemory
+
+describe("should be able create an user", () => {
+
+})
+
+describe("Authenticate a user", () => {
+    beforeEach(() => {
+        userRepositoryInmemory = new UserRepositoryInMemory()
+        authenticateUserUseCase = new AuthenticateUserUseCase(userRepositoryInmemory)
+        createUserUseCase= new CreateUserUseCase(userRepositoryInmemory)
+    })
+
+    it("should be able to authenticate an user",async () => {
+        const user: ICreateUserDTO = {
+            driver_license: "001384",
+            email: "user@example.example",
+            password: "12345",
+            name: "User test"
+        }
+
+        await createUserUseCase.execute(user)
+
+        const result = await authenticateUserUseCase.execute({
+            email: user.email,
+            password: user.password
+        }) 
+
+        expect(result).toHaveProperty('token')
+    })
+
+    it("should not be able to authenticate an nonexistent user",async ()=>{
+        expect(async () => {
+            await authenticateUserUseCase.execute({
+                email: "false@example.example",
+                password: "12345"
+            })
+
+        }).rejects.toBeInstanceOf(AppError)
+    })
+
+})
